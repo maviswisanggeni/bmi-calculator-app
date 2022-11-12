e6eec484a3c86410231972630b15be5b7a6be745
Mavis Daffa Wisanggeni <mavisdaffawisanggeni22@gmail.com>
2022-11-12 16:52:25 +0700

    style: [MW] Change Border Color when Gender clicked

--- lib/constants/constant.dart
+++ lib/constants/constant.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-Color primaryColor = const Color(0xff0a0e21);
+const Color primaryColor = const Color(0xff0a0e21);
 TextStyle? labelTextStyle = TextStyle(
   fontSize: 18,
   color: Colors.white // Color(0xff8d8e98),
--- lib/views/bmi_data_screen.dart
+++ lib/views/bmi_data_screen.dart
@@ -13,6 +13,7 @@ class _BmiDataScreenState extends State<BmiDataScreen> {
   int height = 100;
   int weight = 50;
   int age = 20;
+  String? gender;
 
   double calculateBMI() {
     double heightInMeter = height / 100;
@@ -32,20 +33,35 @@ class _BmiDataScreenState extends State<BmiDataScreen> {
       body: Column(children: [
         Expanded(
           child: Row(
-            children: const [
+            children: [
               Expanded(
-                child: BmiCard(
-                  child: GenderIconText(
-                    icon: Icons.male,
-                    title: 'Male',
+                child: GestureDetector(
+                  onTap: () {
+                    gender = "male";
+                    setState(() {});
+                  },
+                  child: BmiCard(
+                    borderColor:
+                        (gender == "male") ? Colors.white : primaryColor,
+                    child: GenderIconText(
+                      icon: Icons.male,
+                      title: 'Male',
+                    ),
                   ),
                 ),
               ),
               Expanded(
-                child: BmiCard(
-                  child: GenderIconText(
-                    icon: Icons.female,
-                    title: 'Female',
+                child: GestureDetector(
+                  onTap: (){
+                    gender = "female";
+                    setState(() {});
+                  },
+                  child: BmiCard(
+                    borderColor: (gender == "female" ? Colors.white : primaryColor),
+                    child: GenderIconText(
+                      icon: Icons.female,
+                      title: 'Female',
+                    ),
                   ),
                 ),
               ),
@@ -265,9 +281,11 @@ class BmiCard extends StatelessWidget {
   const BmiCard({
     Key? key,
     this.child,
+    this.borderColor = primaryColor,
   }) : super(key: key);
 
   final Widget? child;
+  final Color? borderColor;
 
   @override
   Widget build(BuildContext context) {
@@ -275,6 +293,9 @@ class BmiCard extends StatelessWidget {
       decoration: BoxDecoration(
         color: Color(0xff272a4e),
         borderRadius: BorderRadius.circular(15),
+        border: Border.all(
+          color: borderColor!,
+        ),
       ),
       margin: EdgeInsets.all(15),
       child: child,
